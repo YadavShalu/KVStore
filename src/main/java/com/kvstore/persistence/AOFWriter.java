e44fb8bcf1fb88840f9dca1cc0ebf9d2cfe3888c
shalu-rapid <shaluyadav7118@gmail.com>
2026-04-30 20:38:54 +0530

    feat: add pub/sub with channel-based message broadcasting

---
+++ src/main/java/com/kvstore/persistence/AOFWriter.java
@@ -0,0 +1,41 @@
+package com.kvstore.persistence;
+
+import java.io.IOException;
+
+import com.kvstore.store.DataStore;
+
+import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+public class AOFWriter {
+    private static final String FILE = "appendonly.aof";
+
+    private AOFWriter(){}
+
+    public static synchronized void log(String command){
+        try(FileWriter fw = new FileWriter(FILE, true)){ // true = append mode
+            fw.write(command + "\n");
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void replay() throws IOException{
+        File f = new File(FILE);
+        if(!f.exists())return;
+
+        try(BufferedReader br = new BufferedReader(new FileReader(f))){
+            String line;
+            while((line = br.readLine())!= null){
+                String[] parts = line.split(" ");
+                // Re-execute each commad silently on startup
+
+                if(parts[0].equals("SET")) DataStore.set(parts[1],parts[2]);
+                if (parts[0].equals("DEL"))  DataStore.del(parts[1]);
+            }
+        }
+
+    }
+}
